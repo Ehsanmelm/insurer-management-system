26e7f031c59b4ba9eec46e79200b38c0278c1a5b
ehsan <ehsanmelm82@gmail.com>
2023-07-04 18:29:45 +0330

    create policy related models

---
+++ policyholder/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from insured.models import InsurerModel
+
+# Create your models here.
+
+class CategoryModel(models.Model):
+    category_name = models.CharField(max_length=255)
+    creation_date = models.DateField(default=None , auto_now_add=True)
+
+    def __str__(self) :
+        return f"{self.category_name}"
+    
+class PolicyModel(models.Model):
+    category = models.ForeignKey(CategoryModel , on_delete=models.CASCADE)
+    policy_name = models.CharField(max_length=255)
+    permium = models.IntegerField()
+    ternure = models.IntegerField()
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self) :
+        return f"{self.policy_name}"
+    
+class PolicyRecordModel(models.Model):
+    STATUS_CHOICES = [
+        ("pending" , "Pending"),
+        ("Aproved" , "Aproved"),
+        ("Denied" , "Denied"),
+    ]
+
+    insurer = models.ForeignKey(InsurerModel , on_delete=models.CASCADE)
+    policy =models.ForeignKey(PolicyModel , on_delete=models.CASCADE)
+    status = models.CharField(choices=STATUS_CHOICES , default='Pending')
