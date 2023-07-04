68ac831e11105d78cc4341fbd743712e9ea51475
ehsan <ehsanmelm82@gmail.com>
2023-07-04 17:57:29 +0330

    chore: implement admin panel for insurer

---
+++ insured/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.conf import settings
+from django.contrib import admin
+
+# Create your models here.
+
+
+class InsurerModel(models.Model):
+    user = models.OneToOneField(settings.USER_AUTH_MODEL , on_delete=models.CASCADE)
+    address = models.CharField(max_length=255)
+    phone = models.CharField(max_length=12)
+
+    @admin.display(ordering='user__first_name')
+    def first_name(self):
+        return self.user.first_name
+
+    
+    @admin.display(ordering='user__last_name')
+    def first_name(self):
+        return self.user.last_name
